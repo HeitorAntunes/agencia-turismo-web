2afb05d9bb8f3d4ce7d1ed05d6629bc11c4d6b58
MathCastro <matheusfk74@gmail.com>
2019-11-09 10:23:58 -0300

    bug fixes

--- front-end/agencia-turismo-web/src/screens/LogadoScreen/LogadoScreen.js
+++ front-end/agencia-turismo-web/src/screens/LogadoScreen/LogadoScreen.js
@@ -3,30 +3,32 @@ import PropTypes from "prop-types";
 import styles from "./LogadoScreen.module.css";
 import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
 import CadastroGerenteComponent from "./components/CadastroGerenteComponent/CadastroGerenteComponent";
-import {Switch, Route, useRouteMatch} from "react-router-dom";
-import {withRouter} from 'react-router'
+import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { withRouter } from "react-router";
 
+const LogadoScreen = () => {
+  let match = useRouteMatch();
 
+  const [title, setTitle] = React.useState("")
 
-const LogadoScreen = () => { let match = useRouteMatch();
   return (
-
-  <div>
-    <HeaderComponent> </HeaderComponent>
-    <div className={styles.background}>
-      <div className={styles.content}>
-        <div className={styles.title}> CADASTRO </div>
-        <div className={styles.secondBackground}>
-        <Switch>
-        <Route path={`${match.path}/gerente`}>
-          <CadastroGerenteComponent/>
-        </Route>
-        </Switch>
+    <div>
+      <HeaderComponent> </HeaderComponent>
+      <div className={styles.background}>
+        <div className={styles.content}>
+          <div className={styles.title}> {title} </div>
+          <div className={styles.secondBackground}>
+            <Switch>
+              <Route path={`${match.path}/gerente`}>
+                <CadastroGerenteComponent setTitle={setTitle}/>
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-); }
+  );
+};
 
 LogadoScreen.propTypes = {};
 
